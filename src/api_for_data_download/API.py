e65e4bb824b46d76717fe0ceaf0d1bd14253f7ad
connorcapitolo <connorcapitolo@yahoo.com>
2021-09-16 17:00:41 -0400

    Adding some clarification comments to API.py

--- src/api_for_data_download/API.py
+++ src/api_for_data_download/API.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Libraries Import
 from binance import client
 from binance.client import Client
@@ -15,7 +17,8 @@ api_secret = 'hplmjruPIXcHAQSC6F4OUPwoJvXfwjTeAeqJQiKAh7xAQC9QbZSo0y3yCI4gtbOH'
 
 
 def example_query_account():
-    client = Client(api_key, api_secret)  # not working yet
+    '''still not working, probably since we don't have any transactions'''
+    client = Client(api_key, api_secret)
     info = client.get_account()
     permissions = info['permissions']
     balances = info['balances']
@@ -57,12 +60,15 @@ def time_wrapper(f):
 
 
 def access_all_market():
+    '''Obtaining of list of dictionaries for each of the 1642 coins on Binance; a dictionary is comprised of a symbol and price e.g. {'symbol': 'ETHBTC', 'price': '0.07427400'}
+    '''
     client = Client(api_key, api_secret)
     prices = client.get_all_tickers()
     return prices
 
 
 def order_book(coin='BTCUSDT', limit=5000):
+    '''Provides a dictionary comprised of the keys 'lastUpdateID', 'bids' and 'asks' e.g. {'lastUpdateId': 13693909970, 'bids': [['47047.05000000', '0.96392000'], ['47045.62000000', '0.01432000'], ['47044.69000000', '0.03617000'], ['47044.05000000', '0.04296000'], ['47042.56000000', '0.37185000'], ['47042.55000000', '1.06245000'], ['47042.35000000', '0.00022000'], ['47041.13000000', '0.29406000'], ['47041.12000000', '0.84000000'], ['47039.99000000', '0.85948000']], 'asks': [['47047.06000000', '2.26652000'], ['47050.87000000', '0.02125000'], ['47051.00000000', '0.01060000'], ['47051.40000000', '0.04135000'], ['47052.78000000', '0.00022000'], ['47053.99000000', '0.02806000'], ['47054.00000000', '0.08000000'], ['47054.12000000', '0.01000000'], ['47054.94000000', '0.18917000'], ['47054.95000000', '0.13857000']]}'''
     client = Client(api_key, api_secret)
     depth = client.get_order_book(symbol=coin, limit=limit)
     print(depth)
@@ -70,11 +76,16 @@ def order_book(coin='BTCUSDT', limit=5000):
 
 
 def query_data_for_long_time(coin='BTCUSDT', frequency='1w', request='candles'):
+    '''
+    Getting historical data for a certain coin based on a certain frequency level from the candles feature
+    '''
+    # error checking
     if frequency not in {'1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M'}:
         raise AttributeError(
             'Invalid frequency, should be in {1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M}')
     if request not in {'candles'}:
         raise AttributeError('Invalid request, should be in {candles}')
+    
     client = Client(api_key, api_secret)
     timestamp = client._get_earliest_valid_timestamp(symbol='BTCUSDT', interval=frequency)
     # actual_timestamp = pd.to_timestamp() timestamp - client._get_earliest_valid_timestamp(symbol='BTCUSDT', interval=frequency)
@@ -91,10 +102,15 @@ def query_data_for_long_time(coin='BTCUSDT', frequency='1w', request='candles'):
 
 
 if __name__ == '__main__':
+    print('Executing Script:')
     # dict = order_book()
     # print(len(dict.get('bids')), dict.keys())
     # access_all_market()
-    df = query_data_for_long_time(frequency='6h')
-    print(df.memory_usage().sum())
-    print(df.head())
+    # df = query_data_for_long_time(frequency='6h')
+    # print(df.memory_usage().sum())
+    # print(df.head())
+    # print(len(access_all_market())) # 1642
+    # print(access_all_market()[0])
+    # order_book(limit=10)
+    
 
